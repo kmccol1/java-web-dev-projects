904a31291ad4cb452661ded84437108ec5820430
Kyle McColgan <kdmbus21@gmail.com>
2024-04-18 00:26:42 -0500

    Upload chapter seven textbook example code part two.

--- inheritance/testing-inheritance/exercises/src/main/java/Main.java
+++ inheritance/testing-inheritance/exercises/src/main/java/Main.java
@@ -5,6 +5,11 @@ public class Main
         HouseCat garfield = new HouseCat("Garfield", 12);
         garfield.eat();
         System.out.println(garfield.isTired());   // prints true
+
+        //HouseCat mittens = new HouseCat(8.4);
+
+        HouseCat spike = new HouseCat("Spike");
+        System.out.println(spike.getWeight()); // prints 13
     }
 
 }
