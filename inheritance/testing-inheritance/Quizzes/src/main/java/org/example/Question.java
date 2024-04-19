53b2d446cec7669581d022b61046bc4b249e0435
Kyle McColgan <kdmbus21@gmail.com>
2024-04-18 19:11:07 -0500

    Upload chapter seven studio design part one.

---
+++ inheritance/testing-inheritance/Quizzes/src/main/java/org/example/Question.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Question
+{
+    private int id;
+    private String content;
+    private String type;
+    private String candidateAnswers;
+    private String realAnswer;
+
+
+}
